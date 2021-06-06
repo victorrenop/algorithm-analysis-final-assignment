468401447da7ec53185c400b7e37297b8abfd66a
Victor Poglioni <victorrenop16@gmail.com>
2021-06-06 00:59:12 -0300

    Added the OpenHash functionality and tests.

--- algorithm_analysis_final_assignment/data_structures/hash/open_hash.py
+++ algorithm_analysis_final_assignment/data_structures/hash/open_hash.py
@@ -3,4 +3,33 @@ from algorithm_analysis_final_assignment.data_structures.item import Item
 
 
 class OpenHash(Hash):
-    pass
+    def __init__(self, hash_map_size: int, hash_function_selector: str):
+        self.hash_map_size = hash_map_size
+        self.hash_map = [None] * hash_map_size
+        self.hash_function_selector = hash_function_selector
+
+    def insert(self, key: object, val: int) -> None:
+        idx = 0
+        idx_to_insert = self.hash_function(key, idx)
+        while idx < self.hash_map_size and self.hash_map[idx_to_insert] is not None:
+            idx += 1
+            idx_to_insert = self.hash_function(key, idx)
+        if idx < self.hash_map_size:
+            self.hash_map[idx_to_insert] = Item(key, val)
+            return idx_to_insert
+        return -1
+
+    def search(self, key: int) -> Item:
+        idx = 0
+        idx_to_search = self.hash_function(key, idx)
+        while idx < self.hash_map_size and self.hash_map[idx_to_search].key != key:
+            idx += 1
+            idx_to_search = self.hash_function(key, idx)
+        if idx < self.hash_map_size:
+            return self.hash_map[idx_to_search]
+        return None
+
+    def hash_function(self, key: int, idx: int) -> int:
+        if self.hash_function_selector == "linear":
+            return ((key % self.hash_map_size) + idx) % self.hash_map_size
+        raise NotImplementedError("Hash function not implemented!")
--- tests/unit/data_structures/hash/test_open_hash.py
+++ tests/unit/data_structures/hash/test_open_hash.py
@@ -1,10 +1,60 @@
 from algorithm_analysis_final_assignment.data_structures.hash import OpenHash
+from algorithm_analysis_final_assignment.data_structures import Item
 import pytest
 
 
 class TestOpenHash:
-    def test_insert(self):
-        pass
+    @pytest.mark.parametrize("hash_function", [("linear")])
+    def test_insert(self, hash_function):
+        # prepare
+        hash_map = OpenHash(hash_map_size=11, hash_function_selector=hash_function)
+        items_to_insert = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+        expected_hash_map = [
+            Item(22, 1),
+            Item(88, 1),
+            None,
+            None,
+            Item(4, 1),
+            Item(15, 1),
+            Item(28, 1),
+            Item(17, 1),
+            Item(59, 1),
+            Item(31, 1),
+            Item(10, 1),
+        ]
 
-    def test_search(self):
-        pass
+        # act
+        for item in items_to_insert:
+            hash_map.insert(key=item, val=1)
+        actual_hash_map = hash_map.hash_map
+
+        # assert
+        assert actual_hash_map == expected_hash_map
+
+    @pytest.mark.parametrize(
+        "hash_function,key,expected_result",
+        [
+            ("linear", 22, Item(22, 1)),
+            ("linear", 17, Item(17, 1)),
+        ],
+    )
+    def test_search(self, hash_function, key, expected_result):
+        # prepare
+        hash_map = OpenHash(hash_map_size=11, hash_function_selector=hash_function)
+        items_to_insert = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+
+        # act
+        for item in items_to_insert:
+            hash_map.insert(key=item, val=1)
+        actual_result = hash_map.search(key)
+
+        # assert
+        assert actual_result == expected_result
+
+    def test_hash_function_not_implemented(self):
+        # prepare
+        hash_map = OpenHash(hash_map_size=1, hash_function_selector="not_implemented")
+
+        # act, assert
+        with pytest.raises(NotImplementedError):
+            hash_map.insert(1, 1)
